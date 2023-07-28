0f9a07527183b1274d6721f826e1adc144495b52
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-07-28 15:02:23 +0700

    app(r17): change create.blaade.php page

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,18 +1,20 @@
-@extends('students.layout')
+@extends('persons.layout')
 @section('content')
 
     <div class="card">
-        <div class="card-header">Students Page</div>
+        <div class="card-header">Person Page</div>
         <div class="card-body">
 
-            <form action="{{ url('student') }}" method="post">
+            <form action="{{ url('person') }}" method="post">
                 {!! csrf_field() !!}
-                <label>Name</label></br>
-                <input type="text" name="name" id="name" class="form-control"></br>
-                <label>Address</label></br>
-                <input type="text" name="address" id="address" class="form-control"></br>
-                <label>Mobile</label></br>
-                <input type="text" name="mobile" id="mobile" class="form-control"></br>
+                <label>Nama</label></br>
+                <input type="text" name="nama" id="nama" class="form-control"></br>
+                <label>Jabatan</label></br>
+                <input type="text" name="jabatan" id="jabatan" class="form-control"></br>
+                <label>Jenis Kelamin</label></br>
+                <input type="text" name="jenis_kelamin" id="jenis_kelamin" class="form-control"></br>
+                <label>Alamat</label></br>
+                <input type="text" name="alamat" id="alamat" class="form-control"></br>
                 <input type="submit" value="Save" class="btn btn-success"></br>
             </form>
 
