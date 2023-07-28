c6f89989be1f92685439d222ab113817078b0480
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-07-28 14:56:56 +0700

    app(r17): modify index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,4 @@
-@extends('students.layout')
+@extends('persons.layout')
 @section('content')
     <div class="container">
         <div class="row">
@@ -9,7 +9,7 @@
                         <h2>Laravel 9 Crud</h2>
                     </div>
                     <div class="card-body">
-                        <a href="{{ url('/student/create') }}" class="btn btn-success btn-sm" title="Add New Student">
+                        <a href="{{ url('/person/create') }}" class="btn btn-success btn-sm" title="Add New Person">
                             <i class="fa fa-plus" aria-hidden="true"></i> Add New
                         </a>
                         <br />
@@ -18,36 +18,38 @@
                             <table class="table">
                                 <thead>
                                     <tr>
-                                        <th>#</th>
-                                        <th>Name</th>
-                                        <th>Address</th>
-                                        <th>Mobile</th>
+                                        <th>No.</th>
+                                        <th>Nama</th>
+                                        <th>Jabatan</th>
+                                        <th>Jenis Kelamin</th>
+                                        <th>Alamat</th>
                                         <th>Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($students as $item)
+                                    @foreach ($persons as $item)
                                         <tr>
                                             <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $item->name }}</td>
-                                            <td>{{ $item->address }}</td>
-                                            <td>{{ $item->mobile }}</td>
+                                            <td>{{ $item->nama }}</td>
+                                            <td>{{ $item->jabatan }}</td>
+                                            <td>{{ $item->jenis_kelamin }}</td>
+                                            <td>{{ $item->alamat }}</td>
 
                                             <td>
-                                                <a href="{{ url('/student/' . $item->id) }}" title="View Student"><button
+                                                <a href="{{ url('/person/' . $item->id) }}" title="View Person"><button
                                                         class="btn btn-info btn-sm"><i class="fa fa-eye"
                                                             aria-hidden="true"></i> View</button></a>
-                                                <a href="{{ url('/student/' . $item->id . '/edit') }}"
-                                                    title="Edit Student"><button class="btn btn-primary btn-sm"><i
+                                                <a href="{{ url('/person/' . $item->id . '/edit') }}"
+                                                    title="Edit Person"><button class="btn btn-primary btn-sm"><i
                                                             class="fa fa-pencil-square-o" aria-hidden="true"></i>
                                                         Edit</button></a>
 
-                                                <form method="POST" action="{{ url('/student' . '/' . $item->id) }}"
+                                                <form method="POST" action="{{ url('/person' . '/' . $item->id) }}"
                                                     accept-charset="UTF-8" style="display:inline">
                                                     {{ method_field('DELETE') }}
                                                     {{ csrf_field() }}
                                                     <button type="submit" class="btn btn-danger btn-sm"
-                                                        title="Delete Student"
+                                                        title="Delete Person"
                                                         onclick="return confirm(&quot;Confirm delete?&quot;)"><i
                                                             class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
                                                 </form>
