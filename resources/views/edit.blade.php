90074e995b2983f56705f274fe7b5de7523faa3f
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-07-28 15:00:34 +0700

    app(r17): change edit.blade.php page

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,21 +1,24 @@
-@extends('students.layout')
+@extends('persons.layout')
 @section('content')
 
     <div class="card">
         <div class="card-header">Contactus Page</div>
         <div class="card-body">
 
-            <form action="{{ url('student/' . $students->id) }}" method="post">
+            <form action="{{ url('person/' . $persons->id) }}" method="post">
                 {!! csrf_field() !!}
                 @method('PATCH')
-                <input type="hidden" name="id" id="id" value="{{ $students->id }}" id="id" />
-                <label>Name</label></br>
-                <input type="text" name="name" id="name" value="{{ $students->name }}" class="form-control"></br>
-                <label>Address</label></br>
-                <input type="text" name="address" id="address" value="{{ $students->address }}"
+                <input type="hidden" name="id" id="id" value="{{ $persons->id }}" id="id" />
+                <label>Nama</label></br>
+                <input type="text" name="nama" id="nama" value="{{ $persons->nama }}" class="form-control"></br>
+                <label>Jabatan</label></br>
+                <input type="text" name="jabatan" id="jabatan" value="{{ $persons->jabatan }}"
                     class="form-control"></br>
-                <label>Mobile</label></br>
-                <input type="text" name="mobile" id="mobile" value="{{ $students->mobile }}"
+                <label>Jenis Kelamin</label></br>
+                <input type="text" name="jenis_kelamin" id="jenis_kelamin" value="{{ $persons->jenis_kelamin }}"
+                    class="form-control"></br>
+                <label>Alamat</label></br>
+                <input type="text" name="alamat" id="alamat" value="{{ $persons->alamat }}"
                     class="form-control"></br>
                 <input type="submit" value="Update" class="btn btn-success"></br>
             </form>
